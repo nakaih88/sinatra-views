7ed4d780464c7d48b2c24917f822937881c41682
Nakai <nakai.hamlin18@gmail.com>
2021-04-07 10:49:49 -0400

    commit tree a9df492805bd4b982f270ecdc9591cd1ca5aeabf

--- app.rb
+++ app.rb
@@ -2,7 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+	get '/info' do
+		erb :info
+	  end
+
 	get '/' do
-		
+		erb :index
 	end
+
+	get "/hello" do
+		erb :hello
+	end
+
+	get "/goodbye" do
+		erb :goodbye
+	end
+
+	get "/date" do
+		erb :date
+	end
+
 end
